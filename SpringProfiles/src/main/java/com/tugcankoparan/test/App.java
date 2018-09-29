52f4b1c8e60e2834b9973de3a05b74f91af07f5b
Tuğcan <tugcankoparan10@gmail.com>
2018-09-29 13:10:15 +0300

    Spring Profiles

---
+++ SpringProfiles/src/main/java/com/tugcankoparan/test/App.java
@@ -0,0 +1,30 @@
+package com.tugcankoparan.test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.AbstractEnvironment;
+import com.tugcankoparan.movie.MovieDao;
+
+public class App {
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "live");
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		/*AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.getEnvironment().setActiveProfiles("live");
+		context.register(AppConfig.class);
+		context.refresh();
+		*/
+		MovieDao obj = (MovieDao) context.getBean("movieDao");
+		log.debug("Result : {}", obj.findByDirector("tugcankoparan"));
+		log.debug("Result : {}", obj.findByDirector("tugcankoparan"));
+		log.debug("Result : {}", obj.findByDirector("tugcankoparan"));
+
+		((ConfigurableApplicationContext) context).close();
+
+	}
+}
---
+++ SpringProfiles/src/main/java/com/tugcankoparan/test/AppConfig.java
@@ -0,0 +1,34 @@
+package com.tugcankoparan.test;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@EnableCaching
+@ComponentScan({"com.tugcankoparan.*"})
+public class AppConfig {
+
+	/*private static final Logger log = LoggerFactory.getLogger(AppConfig.class);
+	
+	@Bean
+	@Profile("dev")
+    public CacheManager concurrentMapCacheManager() {
+		log.debug("Cache manager is concurrentMapCacheManager");
+        return new ConcurrentMapCacheManager("movieFindCache");
+    }
+	
+	@Bean
+	@Profile("live")
+	public CacheManager cacheManager() {
+		log.debug("Cache manager is ehCacheCacheManager");
+		return new EhCacheCacheManager(ehCacheCacheManager().getObject());
+	}
+
+	@Bean
+	@Profile("live")
+	public EhCacheManagerFactoryBean ehCacheCacheManager() {
+		EhCacheManagerFactoryBean cmfb = new EhCacheManagerFactoryBean();
+		cmfb.setConfigLocation(new ClassPathResource("ehcache.xml"));
+		cmfb.setShared(true);
+		return cmfb;
+	}*/
+}
